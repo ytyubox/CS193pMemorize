b78bc9151550036d3d4177f00a46c7eff2ba116e
Tsungyu Yu <ytyubox@gmail.com>
2020-05-27 13:12:15 +0800

    add MermonyGame

---
+++ CS193pMemorize/MermonyGame.swift
@@ -0,0 +1,24 @@
+//
+//  MermonyGame.swift
+//  CS193pMemorize
+//
+//  Created by 游宗諭 on 2020/5/27.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import Foundation
+
+struct MermoryGame<CardContent> {
+    
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print(#file,#line, "card chosen:",card)
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
