cef5176daf49c2551c7d730a0761037a331b26d2
Tsungyu Yu <ytyubox@gmail.com>
2020-05-27 12:55:44 +0800

    Initial Commit

---
+++ CS193pMemorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193pMemorize
+//
+//  Created by 游宗諭 on 2020/5/27.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
