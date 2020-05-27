16820872086dedd02c5d57c3009a7b5c24c611f2
Tsungyu Yu <ytyubox@gmail.com>
2020-05-27 13:35:01 +0800

    Shuffle the cards

--- CS193pMemorize/ContentView.swift
+++ CS193pMemorize/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         HStack {
-            ForEach(viewModel.cards) { card in
+            ForEach(viewModel.cards.shuffled()) { card in
                 CardView(card: card)
                     .onTapGesture {
                         self.viewModel.choose(card: card)
